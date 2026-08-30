9355ee3b6cba3319a6025409df702bce96338b99
Dharshini N <dharshini20050916@gmail.com>
2026-08-30 13:34:50 +0530

    Add files via upload

---
+++ Buyandsell.java
@@ -0,0 +1,23 @@
+package Array;
+
+public class Buyandsell {
+    public int maxProfit(int[] prices) {
+         int min = prices[0];
+        int profit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < min) {
+                min = prices[i];
+            }
+
+            int currentProfit = prices[i] - min;
+
+            if (currentProfit > profit) {
+                profit = currentProfit;
+            }
+        }
+
+        return profit;
+    }
+}
+    
