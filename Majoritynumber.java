29c2f48916401c77a275d453c36c24eb83c04ee9
Dharshini N <dharshini20050916@gmail.com>
2026-08-30 17:54:58 +0530

    Add files via upload

---
+++ Majoritynumber.java
@@ -0,0 +1,24 @@
+package Array;
+ class Majoritynumber {
+    public static void main(String[] args) {
+
+        int[] a = {2, 2, 1, 2, 3, 2, 2};
+        int n = a.length;
+
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+
+            for (int j = 0; j < n; j++) {
+                if (a[i] == a[j]) {
+                    count++;
+                }
+            }
+
+            if (count > n / 2) {
+                System.out.println("Majority number: " + a[i]);
+                break;
+            }
+        }
+    }
+}
+
